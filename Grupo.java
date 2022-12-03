b9c2f85ba423e063653a9cebc510723461673279
Lobo250895 <estefania.lobo@davinci.edu.ar>
2022-12-03 16:13:21 -0300

    add constructor

--- Grupo.java
+++ Grupo.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Grupo extends EtapaMundial{
+    private ArrayList<Equipo>EquiposQueAvanzan;
+    public Grupo(String descripcionEtapa, ArrayList<Partido> partidos, ArrayList<Equipo> equiposQueAvanzan,
+                 ArrayList<Equipo> equiposQueAvanzan2) {
+        super(descripcionEtapa, partidos, equiposQueAvanzan);
+        EquiposQueAvanzan = equiposQueAvanzan2;
+    }
+
+    @Override
+    public String toString() {
+        return "Grupo [EquiposQueAvanzan=" + EquiposQueAvanzan + "]";
+    }
+
 }
--- Llave.java
+++ Llave.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Llave extends EtapaMundial{
+    private ArrayList<Equipo> EquiposQueAvanzan;
+
+    public Llave(String descripcionEtapa, ArrayList<Partido> partidos, ArrayList<Equipo> equiposQueAvanzan,
+                 ArrayList<Equipo> equiposQueAvanzan2) {
+        super(descripcionEtapa, partidos, equiposQueAvanzan);
+        EquiposQueAvanzan = equiposQueAvanzan2;
+    }
+
+    @Override
+    public String toString() {
+        return "Llave [EquiposQueAvanzan=" + EquiposQueAvanzan + "]";
+    }
 }
