abff69098bb8c3500ca7f8d9ea9ca583e9732ca1
Lobo250895 <estefania.lobo@davinci.edu.ar>
2022-12-03 15:21:34 -0300

    add mundial

--- EtapaMundial.java
+++ EtapaMundial.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class EtapaMundial {
+    private String descripcionEtapa;
+    private ArrayList<Partido> partidos;
+
+    public String getDescripcionEtapa() {
+        return descripcionEtapa;
+    }
+
+    public void setDescripcionEtapa(String descripcionEtapa) {
+        this.descripcionEtapa = descripcionEtapa;
+    }
+
+    public ArrayList<Partido> getPartidos() {
+        return partidos;
+    }
+
+    public void setPartidos(ArrayList<Partido> partidos) {
+        this.partidos = partidos;
+    }
+
+    public EtapaMundial(String descripcionEtapa, ArrayList<Partido> partidos) {
+        this.descripcionEtapa = descripcionEtapa;
+        this.partidos = partidos;
+    }
+
+    
 }
--- Resultado.java
+++ Resultado.java
@@ -1,5 +1,6 @@
-import java.util.Date;
 public class Resultado {
-    
+    private int golesLocal;
+    private int golesVisitante;
+
 
 }
