f0244bb2fd94e5b0b322a9a44c57bd609f99c477
Lobo250895 <estefania.lobo@davinci.edu.ar>
2022-12-22 13:03:43 -0300

    add corrección de un error en el Main Mundial

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Main {
-    public static <Reportederuedas> void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Mundial Qatar 2022");
 
         Date comienzoDelMundial = new Date (2022, 11, 20);
